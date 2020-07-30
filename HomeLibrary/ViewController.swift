903cf3d6063f608d4d47bd0edca436e1e21deed1
Michal Olejniczak <>
2020-07-30 02:40:27 +0200

    adding scanner

--- HomeLibrary/ViewController.swift
+++ HomeLibrary/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, UISearchBarDelegate {
+class ViewController: UIViewController, UISearchBarDelegate, AVCaptureMetadataOutputObjectsDelegate{
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -16,10 +17,13 @@ class ViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var scannerButton: UIButton!
     @IBOutlet weak var myBooksButton: UIButton!
     @IBOutlet weak var wwwButton: UIButton!
-    @IBOutlet weak var menuBackground: UIImageView!
+    @IBOutlet weak var scannerFrame: UIImageView!
     
     let API_Key = "AIzaSyBtEUfIv8lFx4KDcaDnK99EIPSPVIiJIBA"
     let userDefaults = UserDefaults()
+
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     
     var searchingResults: [Book] = []
     var myBooks: [Book] = []
@@ -37,8 +41,120 @@ class ViewController: UIViewController, UISearchBarDelegate {
         tableView.delegate = self
         tableView.dataSource = self
         startingGraphicSettings()
+        
         loadDatas()
     }
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+    
+    func found(code: String) {
+        print(code)
+        searchLabel.text?.append(code)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    @IBAction func scannerButtonAction(_ sender: Any) {
+        
+        captureSession = AVCaptureSession()
+
+    guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+          let videoInput: AVCaptureDeviceInput
+
+          do {
+              videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+          } catch {
+              return
+          }
+
+          if (captureSession.canAddInput(videoInput)) {
+              captureSession.addInput(videoInput)
+          } else {
+              failed()
+              return
+          }
+
+          let metadataOutput = AVCaptureMetadataOutput()
+
+          if (captureSession.canAddOutput(metadataOutput)) {
+              captureSession.addOutput(metadataOutput)
+
+              metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+              metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
+          } else {
+              failed()
+              return
+          }
+
+          previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+          previewLayer.frame = view.layer.bounds
+          previewLayer.videoGravity = .resizeAspectFill
+          view.layer.addSublayer(previewLayer)
+
+//        self.view.bringSubviewToFront(scannerFrame)
+        self.view.layer.
+            
+          captureSession.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+            searchLabel.text?.removeAll()
+            searchLabel.text?.append(stringValue)
+            searchBarSearchButtonClicked (searchLabel)
+            
+//            captureSession.stopRunning()
+        }
+//        self.dismiss(animated: true)
+        
+        scannerFrame.isHidden = true
+        view.layer.sublayers?.removeLast()
+        
+
+
+//        let layer = view.layer.sublayers?.last
+//        layer.dismiss(animated: true)
+//        scannerButtonAction(().self)
+
+    }
+    
+    
+    
+    
+
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchingResults.removeAll()
@@ -115,6 +231,7 @@ class ViewController: UIViewController, UISearchBarDelegate {
         wwwButton.layer.cornerRadius = 10
         myBooksButton.setTitleColor(.black, for: .normal)
         wwwButton.setTitleColor(.white, for: .normal)
+//        scannerFrame.isHidden = true
     }
     
     @IBAction func myBooksAction(_ sender: Any) {
@@ -180,6 +297,39 @@ class ViewController: UIViewController, UISearchBarDelegate {
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate{
+//
+//    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+//        if let first = metadataObjects.first{
+//            guard let readableObject = first as? AVMetadataMachineReadableCodeObject else{
+//                return
+//            }
+//            guard let stringValue = readableObject.stringValue else{
+//                return
+//            }
+//                print(stringValue)
+//        }else{
+//            print("error barcode")
+//        }
+//    }
+    
+    
+//    func captureOut(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+//        if metadataObjects != nil && metadataObjects.count != nil{
+//            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject
+//            {
+//                if object.type == AVMetadataObject.ObjectType.qr{
+//                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+//                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+//                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler:{ (nil) in
+//                        UIPasteboard.general.string = object.stringValue
+//                    }))
+//                    present(alert, animated: true, completion: nil)
+//
+//                }
+//            }
+//        }
+//    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchingResults.count
     }
